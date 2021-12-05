c9f5bd6e2416aef591251c32cf47ac1a24897f7b
thaiphong1101 <tuthaiphong600@gmail.com>
2021-12-05 17:04:47 +0700

    style menu

--- src/main/java/MavenProject/User/controller/HomeController.java
+++ src/main/java/MavenProject/User/controller/HomeController.java
@@ -19,5 +19,14 @@ public class HomeController {
 		return mv;
 		}
 
-
+	@RequestMapping(value ="/dang-nhap", method = RequestMethod.GET)
+	public ModelAndView Login() {
+		ModelAndView mv = new ModelAndView("user/login");	
+		return mv;
+		}
+	@RequestMapping("/dang-ky")
+	public ModelAndView Register() {
+		ModelAndView mv = new ModelAndView("user/register");	
+		return mv;
+		}
 }
